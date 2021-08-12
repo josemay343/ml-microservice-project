063e81c9b0a8f9176b142ced3837187ee5411190
Jose <jose.may343@hotmail.com>
2021-08-12 12:31:25 -0400

    updated output files & run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,15 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=josemay343/prediction-app:latest
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction-app --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pod
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction-app 8000:80
